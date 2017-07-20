727207eb0a272a56452be524ea4bcb8b72909609
Peter Verboom <p.verboom@invest-online.nl>
2017-07-20 13:37:16 +0200

    Added the TrafficEstimatorService

---
+++ src/Exceptions/KeywordEstimateCountDoesNotMatchException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace InvestOnlineAdWordsApi\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class KeywordEstimateCountDoesNotMatchException
+ * @package InvestOnlineAdWordsApi\Exceptions
+ */
+class KeywordEstimateCountDoesNotMatchException extends RuntimeException
+{
+
+}
---
+++ src/Exceptions/NoKeywordEstimatesException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace InvestOnlineAdWordsApi\Exceptions;
+
+use RuntimeException;
+
+/**
+ * Class NoKeywordEstimatesException
+ * @package InvestOnlineAdWordsApi\Exceptions
+ */
+class NoKeywordEstimatesException extends RuntimeException
+{
+
+}
